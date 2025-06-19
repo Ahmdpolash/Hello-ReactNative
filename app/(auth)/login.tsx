4cc9dfdf4ddd7cade93df394baacc18d1ed1d59a
Ahmdpolash <ahmedpolash732@gmail.com>
2025-06-20 00:07:23 +0600

    learn about pressable components

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,15 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Alert } from "react-native";
 import React from "react";
 import ThemeView from "../../components/ThemeView";
 import Spacer from "../../components/Spacer";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/Color";
+import ThemedButton from "../../components/ThemeButton";
 
 export default function Login() {
+  const handleSubmit = async () => {
+    Alert.alert("Hey this is polash");
+  };
+
   return (
     <ThemeView style={styles.container}>
       <Spacer />
       <Text style={styles.title}> Login to Your Account</Text>
       <Spacer height={100} />
+
+      <ThemedButton style={{}} onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+      </ThemedButton>
 
       <Link href="/register">
         <Text style={{ textAlign: "center" }}>Register instead</Text>
@@ -27,5 +37,15 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
+    backgroundColor: "red",
   },
 });
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text } from "react-native";
+import { Alert, StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
 
 import Spacer from "../../components/Spacer";
 import ThemeView from "../../components/ThemeView";
+import ThemedButton from "../../components/ThemeButton";
 
 const Register = () => {
+  const handleSubmit = async () => {
+    Alert.alert("Hey this is polash");
+  };
   return (
     <ThemeView style={styles.container}>
       <Spacer />
       <Text style={styles.title}>Register an Account</Text>
+
+      <ThemedButton style={{}} onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Register</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/login" replace>
